8912ef2d0b73f64a3f7c686c06a20aa0d468f856
DeveshG7 <74104861+DeveshG7@users.noreply.github.com>
2021-05-13 18:34:20 +0530

    Update umbrella.js

--- umbrella.js
+++ umbrella.js
@@ -10,6 +10,7 @@ class Ground {
       
       this.image=loadImage("images/Walking Frame/walking_1.png")
       this.image2=loadImage("PRO-C43 image/bat/Bestman-01.png")
+      this.image3=loadImage("PRO-C43 image/Walking Frame/batman.png")
 
       this.body = Bodies.circle(this.x, this.y, (this.r/2)-40, options);
       
@@ -20,10 +21,15 @@ class Ground {
       imageMode(CENTER);
       image(this.image, pos.x, pos.y, this.r, this.r);
 
-      if(frameCount>=270){
+      if(frameCount>=220){
 
           image(this.image2,pos.x+10, pos.y+45, 200, 350)
       }
+     
+      if(frameCount>=370){
+
+        image(this.image3,pos.x+40, pos.y+80, 680, 550)
+    }
 
 
     }
